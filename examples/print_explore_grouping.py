f11249bb3c53b4bd6ec2a8bf462c08e23c45ad66
Loris Degioanni <loris@sysdig.com>
2016-03-06 14:45:18 -0800

    new print_explore_grouping example

---
+++ examples/print_explore_grouping.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#
+# Print the user's Explore grouping hierarchy.
+#
+
+import sys
+sys.path.insert(0, '../')
+from sdcclient import SdcClient
+
+#
+# Parse arguments
+#
+if len(sys.argv) != 2:
+    print 'usage: %s <sysdig-token>' % sys.argv[0]
+    print 'You can find your token at https://app.sysdigcloud.com/#/settings/user'
+    sys.exit(0)
+
+sdc_token = sys.argv[1]
+
+#
+# Instantiate the SDC client
+#
+sdclient = SdcClient(sdc_token, 'https://app.sysdigcloud.com')
+
+#
+# Fire the request
+#
+res = sdclient.get_explore_grouping_hierarchy()
+
+#
+# Show the result
+#
+print res[1]
