8ddaf039dc8270e207949fca50c8c5a04076479a
Davide Schiera <davide@sysdig.com>
2016-07-26 14:30:15 -0700

    Improved basic example to retrieve data, with nicer print out.

--- examples/get_data_simple.py
+++ examples/get_data_simple.py
@@ -22,39 +22,79 @@ if len(sys.argv) != 2:
 
 sdc_token = sys.argv[1]
 
-#
-# Instantiate the SDC client
-#
 sdclient = SdcClient(sdc_token)
 
 #
-# Prepare the metrics list. In this case, we have just one metric: cpu.used.percent,
-# without segmentation.
-# The data will be aggregated as average both in time and across the hosts.
+# List of metrics to export. Imagine a SQL data table, with key columns and value columns
+# You just need to specify the ID for keys, and ID with aggregation for values.
 #
-metrics = [
-    {"id": "cpu.used.percent",
-     "aggregations": {
-         "time": "avg",
-         "group": "avg"
-     }
-    }]
+metrics =   [
+                # { "id": "agent.tag.team" },
+                # { "id": "kubernetes.pod.label.name" },
+                # { "id": "agent.tag.env", "aggregations": { "time": "concat", "group": "concat" } },
+                { "id": "cpu.used.percent", "aggregations": { "time": "timeAvg", "group": "avg" } }
+            ]
+filter =    None                        # or None
+start =     -600                        # start timestamp, or lower than 0 for "last X seconds"
+end =       0                           # end timestamp, or 0 for "last X seconds"
+sampling =  60                          # sampling time in seconds for time series, or 0 for aggregated data
 
 #
-# Fire the query.
+# Load data
 #
-res = sdclient.get_data(metrics, # metrics list
-                        -600,   # start_ts = 600 seconds ago
-                        0,      # end_ts = now
-                        60)     # 1 data point per minute
+res = sdclient.get_data(metrics, start, end, sampling, filter = filter)
 
 #
 # Show the result
 #
 if res[0]:
-    data = res[1]
+    #
+    # Read response. The JSON looks like this:
+    #
+    # {
+    #     start: timestamp,
+    #     end: timestamp,
+    #     data: [
+    #         {
+    #             t: timestamp,
+    #             d: [ value1, value2, value3, ... ]
+    #         },
+    #         ...
+    #     ]
+    # }
+    #
+    response = res[1]
+
+    colLen = 25
+
+    #
+    # Print summary (what, when)
+    #
+    start = response['start']
+    end = response['end']
+    data = response['data']
+
+    print 'Data for %s from %d to %d' % (filter if filter else 'everything', start, end)
+    print ''
+
+    #
+    # Print table headers
+    #
+    dataToPrint = ' '.join( [ str(x['id']).ljust(colLen) if len(str(x['id'])) < colLen else str(x['id'])[:(colLen - 3)].ljust(colLen - 3) + '...' for x in metrics  ] )
+    print '%s %s' % ('timestamp'.ljust(colLen), dataToPrint) if sampling > 0 else dataToPrint
+    print ''
+
+    #
+    # Print table body
+    #
+    for d in data:
+        timestamp = d['t'] if sampling > 0 else start
+        values = d['d']
+
+        dataToPrint = ' '.join( [ str(x).ljust(colLen) if len(str(x)) < colLen else str(x)[:(colLen - 3)].ljust(colLen - 3) + '...' for x in values  ] )
+
+        print '%s %s' % ( ('<t: %d>' % (timestamp)).ljust(colLen), dataToPrint ) if sampling > 0 else dataToPrint
+
 else:
     print res[1]
     sys.exit(1)
-
-print data
