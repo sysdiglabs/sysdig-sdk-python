29bcc7e262e5c334b3b9d63cfb20f1070f3a7d47
Phil Rzewski <phil@sysdig.com>
2017-01-03 15:41:21 -0800

    Switch to sphinx.ext.linkcode so docs link to GitHub

--- doc/conf.py
+++ doc/conf.py
@@ -30,7 +30,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.linkcode' ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -153,4 +153,21 @@ texinfo_documents = [
 ]
 
 
+def linkcode_resolve(domain, info):
+    def find_line():
+        obj = sys.modules[info['module']]
+        for part in info['fullname'].split('.'):
+            obj = getattr(obj, part)
+        import inspect
+        fn = inspect.getsourcefile(obj)
+        source, lineno = inspect.findsource(obj)
+        return lineno + 1
 
+    if domain != 'py' or not info['module']:
+        return None
+    #tag = 'master' if 'dev' in release else ('v' + release)
+    url = "https://github.com/draios/python-sdc-client/blob/teams-api/sdcclient/_client.py"
+    try:
+        return url + '#L%d' % find_line()
+    except Exception:
+        return url
