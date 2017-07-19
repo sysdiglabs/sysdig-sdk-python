9b835c3c4d8bc50ae9b37c433b0714f556d55228
Mark Stemm <mark.stemm@sysdig.com>
2017-07-19 08:13:20 -0700

    Use yaml for agent config content (#37)
    * Use yaml for agent config content
    
    set_agents_config.py/get_agents_config.py currently read and write the
    raw json that's inside the api body. This isn't very user friendly.
    
    Change them to work directly with the agent config-as-yaml content
    instead. get_agents_config.py will extract the yaml confg from the json
    response and print it. set_agents_config.py will take the agent config
    from the file on the command line, verify that it can be parsed as yaml,
    wrap it in json, and PUT it.
    
    * Cleanly handle no auto config
    
    If there is no auto config, the response will have an empty list of
    files. Handle that and just print a message that there is no config.
    
    * Add markers around config file
    
    That way you can tell if there's leading/trailing whitespace, or no
    whitespace at all.
    
    * Use app.sysdigcloud.com
    
    Instead of app-staging.sysdigcloud.com.

--- examples/get_agents_config.py
+++ examples/get_agents_config.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Get the sysdig cloud agents configuration as a json file and print it on the screen.
+# Get the sysdig cloud agents configuration as yaml and print it on the screen.
 # Agents configuration settings can be managed in a centralized way through the API
 # This script downloads the settings and its result can be edited and the used from
 # the set_agents_config script.
@@ -8,7 +8,6 @@
 
 import os
 import sys
-import json
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), '..'))
 from sdcclient import SdcClient
 
@@ -25,7 +24,7 @@ sdc_token = sys.argv[1]
 #
 # Instantiate the SDC client
 #
-sdclient = SdcClient(sdc_token, 'https://app-staging.sysdigcloud.com')
+sdclient = SdcClient(sdc_token, 'https://app.sysdigcloud.com')
 
 #
 # Get the configuration
@@ -36,6 +35,12 @@ res = sdclient.get_agents_config()
 # Return the result
 #
 if res[0]:
-        print json.dumps(res[1], indent=2)
+    if not("files" in res[1]) or len(res[1]["files"]) == 0:
+        print "No current auto configuration"
+    else:
+        print "Current contents of config file:"
+        print "--------------------------------"
+        print res[1]["files"][0]["content"]
+        print "--------------------------------"
 else:
     print res[1]
