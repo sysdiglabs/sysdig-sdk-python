333c8fa2fa1865af17c5d7014b84b4c926dceffd
Phil Rzewski <philrz@users.noreply.github.com>
2017-08-10 14:20:48 -0700

    New "get_users" function and corresponding example/automation (#39)
    * Add function to get all users and an example that uses it.
    
    * Have the list_users example run via Travis automation

---
+++ examples/list_users.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#
+# List all the users in a Sysdig Monitor environment. The token you provide must
+# have Admin rights.
+#
+
+import os
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), '..'))
+from sdcclient import SdcClient
+
+#
+# Parse arguments
+#
+if len(sys.argv) != 2:
+    print 'usage: %s <sysdig-token>' % sys.argv[0]
+    print 'You can find your token at https://app.sysdigcloud.com/#/settings/user'
+    print 'For this script to work, the user for the token must have Admin rights'
+    sys.exit(1)
+
+sdc_token = sys.argv[1]
+
+#
+# Instantiate the SDC client
+#
+sdclient = SdcClient(sdc_token, 'https://app.sysdigcloud.com')
+
+#
+# Get the configuration
+#
+res = sdclient.get_users()
+if res[0]:
+    print 'Users\n====='
+    for user in res[1]:
+        print user['username']
+else:
+    print res[1]
+    sys.exit(1)
