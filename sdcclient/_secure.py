bc99c4bb78a577221f1176893b5d4143ccdae27a
Ruben Pollan <meskio@sindominio.net>
2019-02-16 12:25:11 +0100

    Add support to get policy events by id, aggregations and filters

--- sdcclient/_secure.py
+++ sdcclient/_secure.py
@@ -332,9 +332,16 @@ class SdSecureClient(_SdcCommon):
         return [True, ret]
 
     def _get_policy_events_int(self, ctx):
-        policy_events_url = self.url + '/api/policyEvents?from={:d}&to={:d}&offset={}&limit={}'.format(int(ctx['from']), int(ctx['to']), ctx['offset'], ctx['limit'])
-        if 'sampling' in ctx:
-            policy_events_url += '&sampling={:d}'.format(int(ctx['sampling']))
+        policy_events_url = self.url + '/api/policyEvents{id}?from={frm:d}&to={to:d}&offset={offset}&limit={limit}{sampling}{aggregations}{scope}{filter}'.format(
+            id="/%s" % ctx["id"] if "id" in ctx else "",
+            frm=int(ctx['from']),
+            to=int(ctx['to']),
+            offset=ctx['offset'],
+            limit=ctx['limit'],
+            sampling='&sampling=%d' % int(ctx['sampling']) if "sampling" in ctx else "",
+            aggregations='&aggregations=%s' % json.dumps(ctx['aggregations']) if "aggregations" in ctx else "",
+            scope='&scopeFilter=%s' % ctx['scopeFilter'] if "scopeFilter" in ctx else "",
+            filter='&eventFilter=%s' % ctx['eventFilter'] if "eventFilter" in ctx else "")
 
         res = requests.get(policy_events_url, headers=self.hdrs, verify=self.ssl_verify)
         if not self._checkResponse(res):
@@ -345,7 +352,7 @@ class SdSecureClient(_SdcCommon):
 
         return [True, {"ctx": ctx, "data": res.json()}]
 
-    def get_policy_events_range(self, from_sec, to_sec, sampling=None):
+    def get_policy_events_range(self, from_sec, to_sec, sampling=None, aggregations=None, scope_filter=None, event_filter=None):
         '''**Description**
             Fetch all policy events that occurred in the time range [from_sec:to_sec]. This method is used in conjunction
             with :func:`~sdcclient.SdSecureClient.get_more_policy_events` to provide paginated access to policy events.
@@ -354,6 +361,9 @@ class SdSecureClient(_SdcCommon):
             - from_sec: the start of the timerange for which to get events
             - end_sec: the end of the timerange for which to get events
             - sampling: sample all policy events using *sampling* interval.
+            - aggregations: When present it specifies how to aggregate events (sampling does not need to be specified, because when it’s present it automatically means events will be aggregated). This field can either be a list of scope metrics or a list of policyEvents fields but (currently) not a mix of the two. When policy events fields are specified, only these can be used= severity, agentId, containerId, policyId, ruleType.
+            - scope_filter: this is a SysdigMonitor-like filter (e.g “container.image=ubuntu”). When provided, events are filtered by their scope, so only a subset will be returned (e.g. “container.image=ubuntu” will provide only events that have happened on an ubuntu container).
+            - event_filter: this is a SysdigMonitor-like filter (e.g. policyEvent.policyId=3). When provided, events are filtered by some of their properties. Currently the supported set of filters is policyEvent.all(which can be used just with matches, policyEvent.policyId, policyEvent.id, policyEvent.severity, policyEvent.ruleTye, policyEvent.ruleSubtype.
 
         **Success Return Value**
             An array containing:
@@ -372,10 +382,16 @@ class SdSecureClient(_SdcCommon):
 
         if sampling is not None:
             ctx["sampling"] = sampling
+        if aggregations is not None:
+            ctx["aggregations"] = aggregations
+        if scope_filter is not None:
+            ctx["scopeFilter"] = scope_filter
+        if event_filter is not None:
+            ctx["eventFilter"] = event_filter
 
         return self._get_policy_events_int(ctx)
 
-    def get_policy_events_duration(self, duration_sec, sampling=None):
+    def get_policy_events_duration(self, duration_sec, sampling=None, aggregations=None, scope_filter=None, event_filter=None):
         '''**Description**
             Fetch all policy events that occurred in the last duration_sec seconds. This method is used in conjunction with
             :func:`~sdcclient.SdSecureClient.get_more_policy_events` to provide paginated access to policy events.
@@ -383,6 +399,9 @@ class SdSecureClient(_SdcCommon):
         **Arguments**
             - duration_sec: Fetch all policy events that have occurred in the last *duration_sec* seconds.
             - sampling: Sample all policy events using *sampling* interval.
+            - aggregations: When present it specifies how to aggregate events (sampling does not need to be specified, because when it’s present it automatically means events will be aggregated). This field can either be a list of scope metrics or a list of policyEvents fields but (currently) not a mix of the two. When policy events fields are specified, only these can be used= severity, agentId, containerId, policyId, ruleType.
+            - scope_filter: this is a SysdigMonitor-like filter (e.g “container.image=ubuntu”). When provided, events are filtered by their scope, so only a subset will be returned (e.g. “container.image=ubuntu” will provide only events that have happened on an ubuntu container).
+            - event_filter: this is a SysdigMonitor-like filter (e.g. policyEvent.policyId=3). When provided, events are filtered by some of their properties. Currently the supported set of filters is policyEvent.all(which can be used just with matches, policyEvent.policyId, policyEvent.id, policyEvent.severity, policyEvent.ruleTye, policyEvent.ruleSubtype.
 
         **Success Return Value**
             An array containing:
@@ -405,6 +424,97 @@ class SdSecureClient(_SdcCommon):
 
         if sampling is not None:
             ctx["sampling"] = sampling
+        if aggregations is not None:
+            ctx["aggregations"] = aggregations
+        if scope_filter is not None:
+            ctx["scopeFilter"] = scope_filter
+        if event_filter is not None:
+            ctx["eventFilter"] = event_filter
+
+        return self._get_policy_events_int(ctx)
+
+    def get_policy_events_id_range(self, id, from_sec, to_sec, sampling=None, aggregations=None, scope_filter=None, event_filter=None):
+        '''**Description**
+            Fetch all policy events with id that occurred in the time range [from_sec:to_sec]. This method is used in conjunction
+            with :func:`~sdcclient.SdSecureClient.get_more_policy_events` to provide paginated access to policy events.
+
+        **Arguments**
+            - id: the id of the policy events to fetch.
+            - from_sec: the start of the timerange for which to get events
+            - end_sec: the end of the timerange for which to get events
+            - sampling: sample all policy events using *sampling* interval.
+            - scope_filter: this is a SysdigMonitor-like filter (e.g “container.image=ubuntu”). When provided, events are filtered by their scope, so only a subset will be returned (e.g. “container.image=ubuntu” will provide only events that have happened on an ubuntu container).
+            - event_filter: this is a SysdigMonitor-like filter (e.g. policyEvent.policyId=3). When provided, events are filtered by some of their properties. Currently the supported set of filters is policyEvent.all(which can be used just with matches, policyEvent.policyId, policyEvent.id, policyEvent.severity, policyEvent.ruleTye, policyEvent.ruleSubtype.
+            - aggregations: When present it specifies how to aggregate events (sampling does not need to be specified, because when it’s present it automatically means events will be aggregated). This field can either be a list of scope metrics or a list of policyEvents fields but (currently) not a mix of the two. When policy events fields are specified, only these can be used= severity, agentId, containerId, policyId, ruleType.
+
+        **Success Return Value**
+            An array containing:
+              - A context object that should be passed to later calls to get_more_policy_events.
+              - An array of policy events, in JSON format. See :func:`~sdcclient.SdSecureClient.get_more_policy_events`
+                for details on the contents of policy events.
+
+        **Example**
+            `examples/get_secure_policy_events.py <https://github.com/draios/python-sdc-client/blob/master/examples/get_secure_policy_events.py>`_
+
+        '''
+        ctx = {"id": id,
+               "from": int(from_sec) * 1000000,
+               "to": int(to_sec) * 1000000,
+               "offset": 0,
+               "limit": 1000}
+
+        if sampling is not None:
+            ctx["sampling"] = sampling
+        if aggregations is not None:
+            ctx["aggregations"] = aggregations
+        if scope_filter is not None:
+            ctx["scopeFilter"] = scope_filter
+        if event_filter is not None:
+            ctx["eventFilter"] = event_filter
+
+        return self._get_policy_events_int(ctx)
+
+    def get_policy_events_id_duration(self, id, duration_sec, sampling=None, aggregations=None, scope_filter=None, event_filter=None):
+        '''**Description**
+            Fetch all policy events with id that occurred in the last duration_sec seconds. This method is used in conjunction with
+            :func:`~sdcclient.SdSecureClient.get_more_policy_events` to provide paginated access to policy events.
+
+        **Arguments**
+            - id: the id of the policy events to fetch.
+            - duration_sec: Fetch all policy events that have occurred in the last *duration_sec* seconds.
+            - sampling: Sample all policy events using *sampling* interval.
+            - aggregations: When present it specifies how to aggregate events (sampling does not need to be specified, because when it’s present it automatically means events will be aggregated). This field can either be a list of scope metrics or a list of policyEvents fields but (currently) not a mix of the two. When policy events fields are specified, only these can be used= severity, agentId, containerId, policyId, ruleType.
+            - scope_filter: this is a SysdigMonitor-like filter (e.g “container.image=ubuntu”). When provided, events are filtered by their scope, so only a subset will be returned (e.g. “container.image=ubuntu” will provide only events that have happened on an ubuntu container).
+            - event_filter: this is a SysdigMonitor-like filter (e.g. policyEvent.policyId=3). When provided, events are filtered by some of their properties. Currently the supported set of filters is policyEvent.all(which can be used just with matches, policyEvent.policyId, policyEvent.id, policyEvent.severity, policyEvent.ruleTye, policyEvent.ruleSubtype.
+
+        **Success Return Value**
+            An array containing:
+              - A context object that should be passed to later calls to get_more_policy_events.
+              - An array of policy events, in JSON format. See :func:`~sdcclient.SdSecureClient.get_more_policy_events`
+                for details on the contents of policy events.
+
+        **Example**
+            `examples/get_secure_policy_events.py <https://github.com/draios/python-sdc-client/blob/master/examples/get_secure_policy_events.py>`_
+
+        '''
+        epoch = datetime.datetime.utcfromtimestamp(0)
+
+        to_ts = (datetime.datetime.utcnow() - epoch).total_seconds() * 1000 * 1000
+        from_ts = to_ts - (int(duration_sec) * 1000 * 1000)
+        ctx = {"id": id,
+               "to": to_ts,
+               "from": from_ts,
+               "offset": 0,
+               "limit": 1000}
+
+        if sampling is not None:
+            ctx["sampling"] = sampling
+        if aggregations is not None:
+            ctx["aggregations"] = aggregations
+        if scope_filter is not None:
+            ctx["scopeFilter"] = scope_filter
+        if event_filter is not None:
+            ctx["eventFilter"] = event_filter
 
         return self._get_policy_events_int(ctx)
 
