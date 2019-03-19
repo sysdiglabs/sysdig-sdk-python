8057300ff8b933e42d4d9841383265d000263666
Davide Schiera <davide@sysdig.com>
2019-03-19 12:56:49 -0700

    v0.7.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='sdcclient',
-      version='0.6.8',
+      version='0.7.0',
       description='Python client for Sysdig Cloud',
       url='http://github.com/draios/python-sdc-client',
       author='sysdig Inc.',
