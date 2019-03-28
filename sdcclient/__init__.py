b5f0691db093c29229a07dbafd76bd551c48d6d5
davideschiera <davide.schiera@gmail.com>
2019-03-28 15:48:09 -0700

    Move v1 to separate module

--- sdcclient/__init__.py
+++ sdcclient/__init__.py
@@ -1,4 +1,5 @@
 from sdcclient._monitor import SdcClient
 from sdcclient._monitor import SdMonitorClient
+from sdcclient._monitor_v1 import SdMonitorClientV1
 from sdcclient._secure import SdSecureClient
 from sdcclient._scanning import SdScanningClient
