4e573be2686710e8cfe204a547ade89c1fb0ed78
Michael Vittiglio <michael.vittiglio@gmail.com>
2019-07-16 09:55:24 -0400

    Trimmed up the example code.

--- examples/dashboard_basic_crud.py
+++ examples/dashboard_basic_crud.py
@@ -2,7 +2,6 @@
 #
 # Simple example of dashboard creation, retrieval, updating, and deletion.
 #
-
 import os
 import sys
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(sys.argv[0])), '..'))
@@ -74,49 +73,6 @@ ok, res = sdclient.delete_dashboard(dashboard)
 
 #
 # Check for successful delete
-#
-if not ok:
-    print(res)
-    sys.exit(1)
-
-#
-# Create Dashboard.
-#
-ok, res = sdclient.create_dashboard("Sample dashboard - " + uuid.uuid4().hex)
-
-#
-# Check for successful update
-#
-if not ok:
-    print(res)
-    sys.exit(1)
-
-new_dashboard = res['dashboard']
-
-#
-# Update Dashboard with previous data. It will be easier to copy the necessary fields to old dashboard data than the other way around.
-#
-dashboard['id'] = new_dashboard['id']
-dashboard['version'] = new_dashboard['version']
-dashboard['name'] = new_dashboard['name']
-ok, res = sdclient.update_dashboard(dashboard)
-
-#
-# Check for successful update
-#
-if not ok:
-    print(res)
-    sys.exit(1)
-
-new_dashboard = res['dashboard']
-
-#
-# Delete Dashboard.
-#
-ok, res = sdclient.delete_dashboard(new_dashboard)
-
-#
-# Check for successful update
 #
 if not ok:
     print(res)
