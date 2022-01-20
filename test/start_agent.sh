20ee48d608802c54958b38c828d12a832e6a002d
Fede Barcelona <fede_rico_94@hotmail.com>
2022-01-20 17:30:19 +0100

    ci: Update agent installation for the latest version (#203)

--- test/start_agent.sh
+++ test/start_agent.sh
@@ -3,7 +3,7 @@
 set -euxo pipefail
 
 # Start an agent using the testing account API key to send some data
-docker run -d -it --rm --name sysdig-agent --privileged --net host --pid host -e COLLECTOR=collector-staging.sysdigcloud.com -e ACCESS_KEY=$PYTHON_SDC_TEST_ACCESS_KEY -v /var/run/docker.sock:/host/var/run/docker.sock  -v /dev:/host/dev -v /proc:/host/proc:ro -v /boot:/host/boot:ro -v /lib/modules:/host/lib/modules:ro -v /usr:/host/usr:ro --shm-size=350m sysdig/agent
+docker run -d --name sysdig-agent --restart always --privileged --net host --pid host -e ACCESS_KEY=$PYTHON_SDC_TEST_ACCESS_KEY -e COLLECTOR=collector-staging.sysdigcloud.com -e SECURE=true -e TAGS= -v /var/run/docker.sock:/host/var/run/docker.sock -v /dev:/host/dev -v /proc:/host/proc:ro -v /boot:/host/boot:ro -v /lib/modules:/host/lib/modules:ro -v /usr:/host/usr:ro --shm-size=512m quay.io/sysdig/agent
 
 # make sure the agent starts sending data and the backend makes it available via API
 sleep 60
