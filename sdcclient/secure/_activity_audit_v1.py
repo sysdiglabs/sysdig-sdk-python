1bf87ef3862ad23197e4f3f7b8b5cdf283a3843a
Angel Luis Blasco <22562761+alblasco@users.noreply.github.com>
2022-04-20 19:15:20 +0200

    Fix also example

--- sdcclient/secure/_activity_audit_v1.py
+++ sdcclient/secure/_activity_audit_v1.py
@@ -33,7 +33,7 @@ class ActivityAuditClientV1(_SdcCommon):
         Examples:
             >>> client = ActivityAuditClientV1(token=SECURE_TOKEN)
             >>>
-            >>> now = datetime.datetime.utcnow()
+            >>> now = datetime.datetime.now()
             >>> three_days_ago = now - datetime.timedelta(days=3)
             >>> max_event_number_retrieved = 50
             >>> data_sources = [ActivityAuditDataSource.CMD, ActivityAuditDataSource.KUBE_EXEC]
