27b7a151179558ec590f36987c20774117839b7f
Filip Tubic <tubefilip@gmail.com>
2023-06-16 13:34:58 +0200

    fix(team): add test for get all teams

--- specs/_common/team_spec.py
+++ specs/_common/team_spec.py
@@ -4,6 +4,7 @@ from expects import expect, equal
 from mamba import before, description, it
 from sdcclient import SdSecureClient, SdMonitorClient
 from specs import be_successful_api_call
+from collections import defaultdict
 
 TEAM_PREFIX_NAME = 'sysdig-sdk - '
 
@@ -20,6 +21,29 @@ with description("Teams", "integration", "teams") as self:
 
     with before.each:
         self.team_name = f'{TEAM_PREFIX_NAME}{uuid.uuid4()}'
+
+    with it("it should list all teams"):
+        ok, teams_monitor = self.monitor_client.get_teams()
+        expect((ok, teams_monitor)).to(be_successful_api_call)
+
+        ok, teams_secure = self.secure_client.get_teams()
+        expect((ok, teams_secure)).to(be_successful_api_call)
+
+        count_monitor = defaultdict(int)
+        count_secure = defaultdict(int)
+
+        def count_products(teams, count):
+            for team in teams:
+                for product in team['products']:
+                    count[product]+=1
+
+        count_products(teams_monitor, count_monitor)
+        count_products(teams_secure, count_secure)
+
+        expect(len(count_secure)).to(equal(len(count_monitor)))
+        for k, v in count_monitor.items():
+            expect(count_secure[k]).to(equal(v))
+        expect(len(teams_secure)).to(equal(len(teams_monitor)))
 
     with it("it should list only monitor teams"):
         ok, team = self.secure_client.create_team(self.team_name)
